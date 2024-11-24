226548e61f9868eda475f6a04b4f3c26ddd8666c
Ashen Udithamal <udithamal.lk@gmail.com>
2024-11-24 14:12:25 +0530

    Add company reposiory to module

--- src/modules/company/company.module.ts
+++ src/modules/company/company.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyController } from './company.controller';
 import { CompanyService } from './company.service';
+import { Company } from './entities/company.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Company])], // Register the entity
   controllers: [CompanyController],
   providers: [CompanyService],
 })
